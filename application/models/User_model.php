2072cc8a947259b3a0de33bfef15cecb6dae446c
lariyl <34036494+lariyl@users.noreply.github.com>
2017-11-29 20:43:15 +0800

    Initial commit

---
+++ application/models/User_model.php
@@ -0,0 +1,49 @@
+<?php
+class User_model extends CI_model{
+ 
+ 
+ 
+public function register_user($user){
+ 
+ 
+$this->db->insert('users', $user);
+ 
+}
+ 
+public function login_user($username,$pass){
+ 
+  $this->db->select('*');
+  $this->db->from('users');
+  $this->db->where('username',$username);
+  $this->db->where('password',$pass);
+ 
+  if($query=$this->db->get())
+  {
+      return $query->row_array();
+  }
+  else{
+    return false;
+  }
+ 
+ 
+}
+public function username_check($username){
+ 
+  $this->db->select('*');
+  $this->db->from('users');
+  $this->db->where('username',$username);
+  $query=$this->db->get();
+ 
+  if($query->num_rows()>0){
+    return false;
+  }else{
+    return true;
+  }
+ 
+}
+ 
+ 
+}
+ 
+ 
+?>

73bb58c6654d9d9d8d1d4cf0605af8969181bfc8
captainelihu <31009528+captainelihu@users.noreply.github.com>
2018-02-10 23:34:18 +0800

    .

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -32,38 +32,30 @@ Class Auth Extends CI_Controller
 
 	public function verifyLogin()
 	{
-		if(empty($this->input->post('username')) || empty($this->input->post('password')))
+		if(isset($_POST['username']) && isset($_POST['password']))
 		{
-			redirect('/');
-		}
+			$credentials = array(
+				'username'=>$this->input->post('username'),
+				'password'=>md5($this->input->post('password'))
+			);
 
-
-		$credentials = array(
-			'username'=>$this->input->post('username'),
-			'password'=>md5($this->input->post('password'))
-		);
-
-		$login = $this->AuthModel->loginCheck($credentials['username'],$credentials['password']);
-		if($login != false)
-		{
-			$this->session->set_userdata('id',$login['id']);
-			$this->session->set_userdata('username',$login['username']);
-			$this->session->set_userdata('role',$login['role']);
-			redirect('/');
+			$login = $this->AuthModel->loginCheck($credentials['username'],$credentials['password']);
+			if($login != false)
+			{
+				$this->session->set_userdata('id',$login['id']);
+				$this->session->set_userdata('username',$login['username']);
+				$this->session->set_userdata('role',$login['role']);
+				$this->index();
+			}
+			else
+			{
+				$this->session->set_flashdata('error_msg', 'Invalid Username/Password.');
+				redirect('Auth');
+			}
 		}
 		else
 		{
-			//redierct to landing page with error message
-			if(empty($this->session->userdata('id')))
-			 {
-				$this->session->set_flashdata('error_msg', 'Invalid Username/Password.');
-     		 	$this->load->view("LandingPage");
-			 }
-			 else
-			 {
-			 	redirect('/');
-			 }
-
+			redirect('Auth');
 		}
 	}
 

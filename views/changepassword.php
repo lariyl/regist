b4ddfef15cf9dedf791566664f6ef88c49fd8a5d
lariyl <34036494+lariyl@users.noreply.github.com>
2018-02-12 22:38:51 +0800

    .

--- views/changepassword.php
+++ views/changepassword.php
@@ -51,18 +51,18 @@
         <form role="form" method="post" action="<?php echo base_url('auth/updatePassword'); ?>"> 				
           <div class="panel-body">
             <div class="form-group">
-              <?php echo form_password(['name'=>'password', 'placeholder'=>'Password']); ?>
+              <?php echo form_password(['class' => 'form-control', 'name'=>'password', 'placeholder'=>'Password']); ?>
               <?php echo form_error('password', '<div class="text-danger">', '</div>'); ?>
 				    </div>
            <div class="form-group">
-            <?php echo form_password(['name'=>'new_password','placeholder'=>'New Password']); ?>
+            <?php echo form_password(['class' => 'form-control', 'name'=>'new_password','placeholder'=>'New Password']); ?>
             <?php echo form_error('new_password', '<div class="text-danger">', '</div>'); ?>
 				   </div>
            <div class="form-group">
-            <?php echo form_password(['name'=>'confirm_password', 'placeholder'=>'Password Confirmation']); ?>
+            <?php echo form_password(['class' => 'form-control', 'name'=>'confirm_password', 'placeholder'=>'Password Confirmation']); ?>
             <?php echo form_error('confirm_password', '<div class="text-danger">', '</div>'); ?>
            </div>
-            <?php echo form_submit(['name'=>'submit', 'value'=>'Update Password']); ?> 
+            <?php echo form_submit(['class' => 'btn btn-lg btn-success btn-block', 'name'=>'submit', 'value'=>'Update Password']); ?> 
             <?php echo form_close(); ?>	
         </div>
    </body>

8dedeea8844ce02cfdc9665bf41ed5158f7ed420
lariyl <34036494+lariyl@users.noreply.github.com>
2018-02-12 23:56:44 +0800

    UI

--- views/User/index.php
+++ views/User/index.php
@@ -11,6 +11,7 @@
 
 
 
+
 <!-- Sidebar/menu -->
       <nav class="navbar navbar-default">
         <div class="container-fluid">
@@ -19,24 +20,53 @@
           <a href="<?php echo base_url('auth/changePassword');?>"> <button type="button" class="btn btn-default navbar-btn pull-right">Change Password</button></a>
         </div>
       </nav>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <div>
-              <h5>Approved</h5>
-              <select id="course-list" >
-                <?php
-                foreach($list_course as $idx => $lc)
-                {
-                  echo "<option value='$lc->id'>$lc->subject</option>";
-                }
-                ?>
-              </select>
-            </div>
+
+    <div class="container">
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h1 class="page-header">Dashboard</h1>
+      </div>
+        <div class="dropdown" >
+          <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+              <div>
+                <h5>Approved Course</h5>
+                <select id="course-list" >
+                  <?php
+                  foreach($list_course as $idx => $lc)
+                  {
+                    echo "<option value='$lc->id'>$lc->subject</option>";
+                  }
+                  ?>
+                </select>
+             </div>
+
+        <div class="dropdown">
+         <h5>Current Course</h5>
+         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Select Course<span class="caret"></span></button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
           </ul>
         </div>
-      </div>
+
+        <div class="dropdown">
+         <h5>Past Course</h5>
+         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">History Course<span class="caret"></span></button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </div>
+
+           </ul>
+         </div>
+        </div>
     </div>
 
   <script>

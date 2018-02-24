54888a42023f7359de84c635100cad46d7c450c6
lariyl <34036494+lariyl@users.noreply.github.com>
2018-02-24 23:50:48 +0800

    latest

--- views/User/viewReports.php
+++ views/User/viewReports.php
@@ -8,7 +8,6 @@
 th{
 	text-align: center;
 	background-color: slategrey;
-
 }
 </style>
 
@@ -21,29 +20,86 @@ th{
 				<div class="col-md-10 col-md-offset-2 main">
 					<h3 align="center">OBTL Form 4</h3>
 					<h5 align="center">Course Assessment Report</h5>
-<!-- 					<p>Course Code & Title: </p>
-					<p>Name of Program: BS Computer Engineering </p>
- -->					<table class="table table-bordered">
+					<p class="pull-left"><b>Course Code & Title:</b> CPE 01TN |  COMPUTER HARDWARE FUNDAMENTALS | Grp.1</p>
+					<p class="pull-right"><b>Name of Faculty: </b>Juan De La Cruz</p>
+					<p class="pull-right"><b>Term & AY: </b>Second Semester, AY 2016-2017</p>
+					<p class="pull-left"><b>Name of Program: </b>BS Computer Engineering</p>
+					<table class="table table-bordered">
 						<thead class="table">
 							<tr>
 								<th scope="col">Course Outcome</th>
 								<th scope="col">Assesment Task</th>
-								<th scope="col">Student's Level of Achievement of Outcome <br> (Frequency, Percentage) </th>
+								<th colspan="4" scope="col">Student's Level of Achievement of Outcome <br> (Frequency, Percentage)</th>
 								<th scope="col">Target</th>
 								<th scope="col">Gap</th>
 							</tr>
 						</thead>
 						<tbody>
 						    <tr align="center">
-						      <td></td>
-						      <td></td>
-						      <td>1.0</td>
-						      <td></td>						      
+						    	<td></td>
+						    	<td></td>			      
+						    	<td>1.0</td>
+						    	<td>2.0</td>			      
+						    	<td>3.0</td>
+						    	<td>4.0</td>			      
+						    	<td></td>
+						    	<td></td>			      
 						    </tr>
-
-
 						</tbody>   
 					</table>	 
+
+					<div class="container-fluid">
+						<p class="pull-left"><b>1.0 - Exceeds Expections</b> (evaluation of output may range from 1.0 to 1.4)</p>
+						<p class="pull-right"><b>3.0 - Partially Meet Expecttions</b> (evaluation of output may range from 2.5 to 3.0)</p>
+						<p class="pull-right"><b>4.0 - Does Not Meet Expecttions</b> (evaluation of output with a rating 3.0 or lower)</p>
+						<p class="pull-left"><b>2.0 - Meets Expections</b> (evaluation of output may range from 1.5 to 2.4)</p>
+					</div>
+
+					<table class="table table-bordered" style="width: 25%;">
+						<thead class="table">
+							<tr>
+								<td>Total No. of Students Enrolled:</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Number of Students Passed:</td>
+							</tr>
+						</tbody>
+					</table>
+
+
+					<div class="container-fluid">
+						<div class="row">
+							<div>
+								<div class="panel panel-default">
+									<div class="panel-heading">
+										<h3 class="panel-title">Continuos Quality Improvement</h3>
+									</div>
+									<div class="panel-body">
+										<h5><u>Data Interpretation</u></h5>
+										<div class="form-group">
+										  <textarea class="form-control" rows="5" id="comment"></textarea>
+										</div>
+										<h5><u>Proposed Improvement Actions</u></h5>
+										<h6>In order to increase the course's performance improvement actions are recommended:</h6>
+										<div class="form-group">
+										  <textarea class="form-control" rows="5" id="comment"></textarea>
+										</div>
+									  	<div class="col-xs-4 pull-right">
+											<label for="received_by">Received By:</label>
+									    	<input class="form-control" id="received_by" type="text">
+									  	</div>
+										<div class="col-xs-2 pull-right">
+											<label for="date_submitted">Date Submitted:</label>
+									    	<input class="form-control" id="date_submitted" type="time">	
+									  	</div>	  	
+									</div>
+								<button type="submit" class="btn btn-primary pull-right">Submit Report</button>	
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
